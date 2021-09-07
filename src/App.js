1824050e7491090facf82b23f86cac169b8c1ebf
shazan99 <shazanahmed786@gmail.com>
2021-09-07 16:11:31 +0530

    Adding API to the app to fetch country data
    Signed-off-by: shazan99 <shazanahmed786@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+   const getCountriesData = async () => {
+     await fetch("https://disease.sh/v3/covid-19/countries")
+     .then((response) => response.json())
+     .then((data) => {
+       const countries = data.map((country) => ({
+           name: country.country, //United States, United Kingdom
+           value: country.countryInfo.iso2, //UK, USA, INdia
+         })); 
+
+         setCountries(countries);
+     });
+   };
+
+   getCountriesData();
+  }, []);  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <div className="app__header">
+     <h1>Covid-19 Tracker</h1>
+     <FormControl className="app__dropdown">
+        <Select variant="outlined" value="abc" >
+          {/* Loop through all the countries and show a drop down
+          list of the options */}
+          {
+            countries.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+         </Select>
+      </FormControl>
+     </div>
+      
+      
+
+      {/* Header */}
+      {/* Title + Select input dropdown feild */}
+
+      {/* Infoboxes */}
+      {/* Infoboxes */}
+      {/* Infoboxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+      
     </div>
   );
 }
